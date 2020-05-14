0a2491c4cd6d15121857194ca70fc58d0b4c14c0
kunal <31889898+chalotrakunal@users.noreply.github.com>
2020-05-14 17:37:30 +0530

    first commit
    done!

---
+++ Matrix.cs
@@ -0,0 +1,123 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp17
+{
+    public enum DisplayType
+    {
+       All=0,
+       Odd,
+       Even
+    };
+    class Matrix
+    {
+        private int _rows;
+        private int _columns;
+        private int[,] array;
+        public Matrix(int rows, int columns)
+        {
+            _rows = rows;
+            _columns = columns;
+            array = new int[_rows, _columns];
+        }
+        public void Read()
+        {
+            for (int row = 0; row < _rows; row++)
+            {
+                for (int column = 0; column < _columns; column++)
+                {
+                    Console.WriteLine("enter the element into matrix at postion: " + "[" + row + "]" + "[" + column + "]");
+                    array[row, column] = int.Parse(Console.ReadLine());
+                }
+            }
+        }
+        private int sumOfOddElementsOfMatrix = 0;
+        public int GetSumofOddElements()
+        {
+            return sumOfOddElementsOfMatrix;
+        }
+        private int sumOfEvenElementsOfMatrix = 0;
+        public int GetSumofEvenElements()
+        {
+            return sumOfEvenElementsOfMatrix;
+        }
+        public void SumOfBothEvenOddElements()
+        {
+            for(int row =0; row<_rows;row++)
+            {
+                for(int column =0; column<_columns;column++)
+                {
+                    if(array[row,column]%2==0)
+                    {
+                        sumOfEvenElementsOfMatrix += array[row, column];
+                    }
+                    else
+                    {
+                        sumOfOddElementsOfMatrix += array[row, column];
+                    }
+                }
+            }
+        }
+        public void Display(DisplayType type)
+        {
+            for (int row = 0; row < _rows; row++)
+            {
+                string tab = "|\t";
+                for (int column = 0; column < _columns; column++)
+                {
+                    bool isOddEven = array[row, column] % 2 == 0 ? true : false;
+                    switch (type)
+                    {
+                        case DisplayType.Even:
+                            tab += SwitchCase(row, column, isOddEven);
+                            break;
+
+                        case DisplayType.Odd:
+                            tab += SwitchCase(row, column, !isOddEven);
+                            break;
+
+                        case DisplayType.All:
+                            tab = tab + array[row, column].ToString() + "\t";
+                            break;
+
+                        default:
+                            Console.WriteLine("wrong enaum type entered");
+                            break;
+                    }
+                }
+                Console.WriteLine(tab + "|");
+            }
+        }
+        public string SwitchCase(int row, int column, bool isOddEven)
+        {
+            string tab = String.Empty;
+            if (isOddEven)
+            {
+                tab = tab + array[row, column].ToString() + "\t";
+            }
+            else
+            {
+                tab = tab + "#" + "\t";
+            }
+            return tab;
+        }
+        public static Matrix operator *(Matrix matrix1, Matrix matrix2)
+        {
+            Matrix product = new Matrix(matrix1._rows, matrix2._columns);
+            for (int row = 0; row < matrix1._rows; row++)
+            {
+                for (int column = 0; column < matrix2._columns; column++)
+                {
+                    for (int middle = 0; middle < matrix1._columns; middle++)
+                    {
+                        product.array[row, column] += matrix1.array[row, middle] * matrix2.array[middle, column];
+                    }
+                }
+            }
+            return product;
+        }       
+    }
+}
---
+++ MatrixMultiplication.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp17
+{
+    class MatrixMultiplication
+    {
+        public void InputMatrixAndDisplay()
+        {
+            Console.WriteLine("enter the no of rows and columns for first matrix ");
+            int noOfRowsOfMatrix1 = int.Parse(Console.ReadLine());
+            int noOfColumnsOfMatrix1 = int.Parse(Console.ReadLine());
+            Console.WriteLine("enter the no of rows and columns for second matrix ");
+            int noOfRowsOfMatrix2 = int.Parse(Console.ReadLine());
+            int noOfColumnsOfMatrix2 = int.Parse(Console.ReadLine());
+            if(noOfColumnsOfMatrix1==noOfRowsOfMatrix2)
+            {
+                Matrix matrix1 = new Matrix(noOfRowsOfMatrix1, noOfColumnsOfMatrix1);
+                Matrix matrix2 = new Matrix(noOfRowsOfMatrix2, noOfColumnsOfMatrix2);
+                Console.WriteLine("Enter the elements of matrix1 ");
+                matrix1.Read();
+                Console.WriteLine("Enter the elements of matrix2 ");
+                matrix2.Read();
+                DisplayMatrix(matrix1, matrix2, noOfRowsOfMatrix1, noOfColumnsOfMatrix2);
+            }
+            else
+            {
+                Console.WriteLine("Entered wrong dimension of matrix matrix multiplication not possible");
+            }
+        }
+        public void DisplayMatrix(Matrix matrix1, Matrix matrix2, int noOfRowsOfMatrix1, int noOfColumnsOfMatrix2)
+        {
+            Console.WriteLine("----------------------------------------------------------------------------------");
+            Console.WriteLine("Display matrix1 below :");
+            Console.WriteLine("----------------------------------------------------------------------------------");
+            matrix1.Display(DisplayType.All);
+            Console.WriteLine("----------------------------------------------------------------------------------");
+            Console.WriteLine("Display matrix2 below :");
+            Console.WriteLine("----------------------------------------------------------------------------------");
+            matrix2.Display(DisplayType.All);
+            Console.WriteLine("----------------------------------------------------------------------------------");
+            Matrix  product = matrix1 * matrix2;
+            Console.WriteLine("Product of matrix 1 and matrix 2 is below :");
+            Console.WriteLine("----------------------------------------------------------------------------------");
+            product.Display(DisplayType.All);
+            Console.WriteLine("----------------------------------------------------------------------------------");
+            Console.WriteLine("Display Even elements of matrix below : ");
+            Console.WriteLine("----------------------------------------------------------------------------------");
+            product.Display(DisplayType.Even);
+            Console.WriteLine("----------------------------------------------------------------------------------");
+            Console.WriteLine("Display odd elements of matrix below :");
+            Console.WriteLine("----------------------------------------------------------------------------------");
+            product.Display(DisplayType.Odd);
+            Console.WriteLine("----------------------------------------------------------------------------------");
+            product.SumOfBothEvenOddElements();
+            Console.WriteLine("Sum of even elements of matrix  is :" + product.GetSumofEvenElements());
+            Console.WriteLine("Sum of Odd elements of matrix is :" + product.GetSumofOddElements());
+        }
+    }
+}
